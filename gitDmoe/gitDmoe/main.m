0777b81d0bee9d0fd9804b37d29000c47a0d11c0
lizihe <1547123542@qq.com>
2017-06-13 15:56:01 +0800

    修改内容

---
+++ gitDmoe/gitDmoe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitDmoe
+//
+//  Created by luo on 17/6/13.
+//  Copyright © 2017年 VCredit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
